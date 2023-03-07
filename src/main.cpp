eba1b053833bfb21c37576bfd1fe281e3800091c
RandyGaul <randypgaul@gmail.com>
2023-03-06 16:24:37 -0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <cute.h>
+using namespace Cute;
+
+int main(int argc, const char** argv)
+{
+	// Create a window with a resolution of 640 x 480.
+	int options = APP_OPTIONS_DEFAULT_GFX_CONTEXT | APP_OPTIONS_WINDOW_POS_CENTERED;
+	Result result = make_app("Fancy Window Title", 0, 0, 640, 480, options, argv[0]);
+	if (is_error(result)) return -1;
+
+	while (app_is_running())
+	{
+		app_update();
+		// All your game logic and updates go here...
+		app_draw_onto_screen();
+	}
+
+	destroy_app();
+
+	return 0;
+}
